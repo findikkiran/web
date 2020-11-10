abed898d5cff9ae8f67cc3f9193c33a335659989
findikkiran <emrekulaksiz1999@gmail.com>
2020-11-10 21:40:00 +0300

    webproje

---
+++ data.js
@@ -0,0 +1,57 @@
+
+var cards=[
+    { 
+        name:"Tony Hawk's Pro Skater...",
+        corporation:'Vicarious Visions | Activis...',
+        discount:'-40%',
+        price:'200,00$',
+        discount_price:'120,00$',
+        img:'./Discount/tonyhawk.jpg',
+
+
+
+
+    },
+    { 
+        name:"Superliminal Double-Albu...",
+        corporation:'Pillow Castle',
+        discount:'-20%',
+        price:'100,00',
+        discount_price:'80,00',
+        img:'./Discount/superliminal.jpg',
+
+
+
+
+    },
+    { 
+        name:"Auto Chess",
+        corporation:'Dragonest Game',
+        discount:'-100%',
+        price:'80,00',
+        discount_price:'free',
+        img:'./Discount/autochess.jpg',
+
+
+
+
+    },
+    { 
+        name:"Watch Dogs: Legion",
+        corporation:'Ubisoft',
+        discount:'-50%',
+        price:'300,00',
+        discount_price:'150,00',
+        img:'./Discount/watchdosg.jpg',
+
+
+
+
+    }
+
+
+
+
+
+]
+export default cards;
